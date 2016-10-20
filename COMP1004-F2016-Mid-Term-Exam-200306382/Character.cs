3d1670f834f0ef3f7f8b25ed0e53ed69ce409138
Dilpreet Singh <preet19@outlook.com>
2016-10-20 16:20:51 -0400

    Add project files.

---
+++ COMP1004-F2016-Mid-Term-Exam-200306382/Character.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP1004_F2016_Mid_Term_Exam_200306382
+{
+    public class Character
+    {
+        public string Strength { get; set; }
+
+        public string Dexterity { get; set; }
+
+        public string Constitution { get; set; }
+
+        public string Intelligence { get; set; }
+
+        public string Wisdom { get; set; }
+
+        public string Charisma { get; set; }
+
+        public string Race { get; set; }
+    }
+}
---
+++ COMP1004-F2016-Mid-Term-Exam-200306382/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_F2016_Mid_Term_Exam_200306382
+{
+    public static class Program
+    {
+        public static Character character = new Character();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AbilityGeneratorForm());
+        }
+    }
+}
